f6510c745edbbfa3e5d56b76a13eb9a69cf0ad92
meldianing <35475106+meldianing@users.noreply.github.com>
2018-02-01 11:28:51 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>workshop2</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-	<img src = "Image_63.png">
-	<img src = "Image_64.png">
-</body>
-</html>
+<div class = "mainBase">
+	<div class = "centerDiv">
+		<!--Second Layer | Pager-->
+		<div class = "headerDiv">
+			<table>
+				<tr>
+					<td><img style="position: relative; top: 0px;margin: 0px;padding: 0px;" src="img/2.jpg" width="100px" height="90px"></td>
+				</tr>
+			</table>
+		</div>
+		<!--Menu Item and Search Bar-->
+		<div class = "menuItem">
+			<a style="float:left;"; class = "linker" href="php/About.php" id = "about">Cisco Paketracer</a>
+			<a style="float:left;"; class = "linker1" href="php/cabling.php" id = "profile">about</a>
+			<a style="float:left;"; class = "linker2" href="php/fiber.php" id = "contact" href="">network cabbles</a>
+			<a style="float:left;"; class = "linker3" href="php/packet.php" id = "signup" href="">network tools</a>
+		</div>
+		
+	</div>
+	<img src="img/1.jpg" width="100%" height="100%">
+	<!--Login Base-->
